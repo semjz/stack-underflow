3fb25df15332798af0b00299ba22cf5050e92079
semjz <sam.malakjan@gmail.com>
2023-09-22 14:57:12 +0330

    Create users forms

--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,39 @@
 from django import forms
-
 from .models import User
 
 
 class UserUpdateForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = User
+        fields = ["username", "email", "phone_number", "national_id"]
+
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("This username is already exist!")
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if User.objects.filter(emai=email).exists():
+            raise forms.ValidationError("This email is already exist!")
 
 
 class UserRegisterForm(UserUpdateForm):
-    pass
+    class Meta:
+        model = User
+        fields = ["username", "email", "phone_number", "national_id", "password"]
+
+    confirm_password = forms.CharField(max_length=128)
+
+    def clean_confirm_password(self):
+        password = self.cleaned_data["password"]
+        confirm_password = self.cleaned_data["confirm_password"]
+        if confirm_password != password:
+            raise forms.ValidationError("Password must match ")
+        if not confirm_password or not password:
+            raise forms.ValidationError("Enter the both of password")
 
 
 class UserLoginForm(forms.Form):
-    pass
+    username = forms.CharField(max_length=15, required=True)
+    password = forms.CharField(widget=forms.PasswordInput, required=True)
