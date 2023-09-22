2dc54623834195a82d3389cedc678fd61b643b9a
semjz <sam.malakjan@gmail.com>
2023-09-22 14:56:36 +0330

    Create User models

--- users/models.py
+++ users/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator
 
 
-# Create your models here.
 class User(AbstractUser):
-    pass
+    phone_number = models.CharField(max_length=11, validators=[MinLengthValidator(11)])
+    national_id = models.CharField(max_length=10, validators=[MinLengthValidator(10)])
