bcd044d96f56a181c4461cfd02fb37aed5fb650c
semjz <sam.malakjan@gmail.com>
2023-09-22 14:56:59 +0330

    Create questions models

--- questions/models.py
+++ questions/models.py
@@ -1,15 +1,25 @@
 from django.db import models
+from users.models import User
 from django.urls import reverse
 
 
-# Create your models here.
-class Question(models.Model):
-    pass
-
-
 class Tag(models.Model):
-    pass
+    name = models.CharField(max_length=50)
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+    vote = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Answer(models.Model):
-    pass
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    body = models.TextField()
+    vote = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
