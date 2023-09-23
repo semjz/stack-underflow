d8526f507cb6e010b0ec6a287f1eb98e6f8ecfb7
semjz <sam.malakjan@gmail.com>
2023-09-23 19:10:42 +0330

    Hash password in user form modify the modelform save method to hash the password and save it and make the email and field required. add custom validation for password fields if they are empty or not the same.

--- users/forms.py
+++ users/forms.py
@@ -7,15 +7,29 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ["username", "email", "phone_number", "national_id"]
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].required = True
+
     def clean_username(self):
         username = self.cleaned_data["username"]
         if User.objects.filter(username=username).exists():
             raise forms.ValidationError("This username is already exist!")
+        return username
 
     def clean_email(self):
         email = self.cleaned_data["email"]
-        if User.objects.filter(emai=email).exists():
+        if User.objects.filter(email=email).exists():
             raise forms.ValidationError("This email is already exist!")
+        return email
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if self.cleaned_data.get("password"):
+            user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
 
 
 class UserRegisterForm(UserUpdateForm):
@@ -23,15 +37,22 @@ class UserRegisterForm(UserUpdateForm):
         model = User
         fields = ["username", "email", "phone_number", "national_id", "password"]
 
-    confirm_password = forms.CharField(max_length=128)
+    confirm_password = forms.CharField(max_length=128, widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password'].widget = forms.PasswordInput()
+        self.fields['password'].required = False
+        self.fields['confirm_password'].required = False
 
     def clean_confirm_password(self):
         password = self.cleaned_data["password"]
         confirm_password = self.cleaned_data["confirm_password"]
+        if not (confirm_password and password):
+            raise forms.ValidationError("Enter the both of password")
         if confirm_password != password:
             raise forms.ValidationError("Password must match ")
-        if not confirm_password or not password:
-            raise forms.ValidationError("Enter the both of password")
+        return confirm_password
 
 
 class UserLoginForm(forms.Form):
