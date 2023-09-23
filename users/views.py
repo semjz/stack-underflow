5210575a353ee21ee5c8dd3a9354ed248419838f
semjz <sam.malakjan@gmail.com>
2023-09-23 19:13:30 +0330

    Complete User views

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import logout, authenticate, login
 
@@ -8,23 +8,58 @@ from users.models import User
 
 # Create your views here.
 def user_register_view(request):
-    pass
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, "you registered successfully")
+            login(request, user)
+            return redirect("users:home")
+        else:
+            messages.warning(request, "register unsuccessfully")
+    else:
+        form = UserRegisterForm()
+
+    return render(request, "user/user_register_form.html", {"form": form})
 
 
 def user_login_view(request):
-    pass
+    if request.method == "POST":
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "login successful")
+                return redirect("users:home")
+            else:
+                messages.warning(request, "register unsuccessfully")
+    else:
+        form = UserLoginForm()
+
+    return render(request, "user/user_login_form.html", {"form": form})
 
 
 def user_logout_view(request):
-    pass
+    logout(request)
+    return redirect("users:home")
 
 
 def user_profile_view(request, uid):
-    pass
+    user = User.objects.get(id=uid)
+    return render(request, "user/user_profile_view.html", {"user": user})
 
 
 def user_profile_edit_view(request):
-    pass
+    if request.method == "POST":
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserUpdateForm()
+    return render(request, "user/user_edit.html", {"form": form})
 
 
 def user_home_view(request):
