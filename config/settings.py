5b02637689ad26da4d1c2bd88df8a0f8de4afaf6
semjz <sam.malakjan@gmail.com>
2023-09-23 19:13:11 +0330

    Add database for testing

--- config/settings.py
+++ config/settings.py
@@ -79,7 +79,11 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-    }
+    },
+    'test': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'test_db.sqlite3',
+    },
 }
 
 AUTH_USER_MODEL = "users.User"
